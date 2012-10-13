5c194ccde45596a9ee0ba8c3a19d66bef4a3d0a7
Robert Boone <robert.l.boone@gmail.com>
2012-10-13 12:26:59 -0500

    Initial Commit
    Fresh rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rlb3Resume::Application.config.secret_token = 'e7fcdf0bb2347fd9070974bc77fa2f1a3f51fd836432f3ae993c2c16dc74f378e6063916466ea771296060c3f238b267a549d6ff33daff318407a2c5adbad63b'
