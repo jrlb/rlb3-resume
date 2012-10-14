acacfa2ca7fd00fb839d0e141fb33f89454a3d26
Robert Boone <robert.l.boone@gmail.com>
2012-10-13 20:00:31 -0500

    Remove active_record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -51,7 +54,7 @@ module Rlb3Resume
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -62,15 +65,15 @@ module Rlb3Resume
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :domain               => "",
-      :user_name            => "",
-      :password             => "",
+      :domain               => "rlb3.com",
+      :user_name            => "resume@rlb3.com",
+      :password             => "rb1993012",
       :authentication       => :plain,
       :enable_starttls_auto => true
     }
 
     config.action_mailer.default_url_options = {
-      :host => "yourdomain.dev"
+      :host => "blackgeek.me"
     }
   end
 end
