d35d305289c7778d37dfbabd0441340eb9de83a6
Robert Boone <robert@jrlbconsulting.com>
2012-10-28 15:05:03 -0500

    Move index into app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rlb3Resume::Application.routes.draw do
+  root :to => 'home#index'
+  get "home/index"
+
   match 'contact' => 'contact#create', :as => 'contact', :via => :post
   # The priority is based upon order of creation:
   # first created -> highest priority.
