acd4297c21ec115a3dc8e57383443c08bc5c9106
Robert Boone <robert.l.boone@gmail.com>
2012-10-30 12:25:44 -0500

    Cleanup of code and css file

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,12 +1,11 @@
 class ContactController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
-
-        if @contact.valid?
-          ContactsMailer.new_contact(@contact).deliver
-          render :text => "ok"
-        else
-          render :text => "error"
-        end
+      if @contact.valid?
+        ContactsMailer.new_contact(@contact).deliver
+        render :text => "ok"
+      else
+        render :text => "error"
+      end
   end
 end
