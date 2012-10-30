4c30580558d34f3a46cdcccd3c87bc94287debfb
Robert Boone <robert.l.boone@gmail.com>
2012-10-30 08:42:46 -0500

    Add models instances to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @contact = Contact.new
+    @profile = Profile.first
+    @education = Education.all
+    @technical_skills = TechnicalSkills.all
+    @work = Work.all
   end
 end
