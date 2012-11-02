a4b15fb424527dc8111b65bbe30539c1f54e7798
Robert Boone <robert@jrlbconsulting.com>
2012-11-01 23:18:00 -0500

    Sort technical skills by years

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @contact = Contact.new
     @profile = Profile.first
     @education = Education.all
-    @technical_skills = TechnicalSkills.all
+    @technical_skills = TechnicalSkills.all.sort_by {|ts| ts.years.to_i }.reverse
     @work = Work.all
   end
 end
