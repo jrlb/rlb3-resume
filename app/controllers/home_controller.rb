0808468cb8ba836f82af3a17f1eba21b92ff0d8c
Robert Boone <robert@jrlbconsulting.com>
2012-11-01 23:31:59 -0500

    Sort work by year

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
     @profile = Profile.first
     @education = Education.all
     @technical_skills = TechnicalSkills.all.sort_by {|ts| ts.years.to_i }.reverse
-    @work = Work.all
+    @work = Work.all.sort_by {|w| w.year.to_i }.reverse
   end
 end
